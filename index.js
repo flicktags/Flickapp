74618fcfa0351d62afc5a2d2503077d8a8c34654
MFahim786 <132273833+MFahim786@users.noreply.github.com>
2024-01-14 23:25:26 +0500

    Update index.js

--- index.js
+++ index.js
@@ -7,18 +7,24 @@ const port = 3000;
 const user=require('./Api/Routes/user_api')
 const social_media=require('./Api/Routes/social_media')
 const userImage=require('./Api/Routes/index')
-const flickCode=require('./Api/Routes/flickCodes')
 const bodyparser=require('body-parser');
 app.use(bodyparser.urlencoded({extended:false}));
 app.use(bodyparser.json());
 app.use('/user', user); 
 app.use('/socialmedia', social_media);
 app.use('/userImage',userImage)
-app.use('/flickCode',flickCode);
-app.use(express.static('public'));
- 
+ // Handle 404 - Route Not Found
+ const allowedOrigins = ['http://localhost:3000', 'http://localhost:3001'];
 
-app.use(cors());
+app.use(cors({
+  origin: function (origin, callback) {
+    if (!origin || allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  },
+}));
 
  app.use((req, res, next) => { 
   res.status(404).json({
