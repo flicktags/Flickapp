98c1f409ecdfbc1c086edad5467bb88a200017d6
MFahim786 <132273833+MFahim786@users.noreply.github.com>
2024-01-14 23:14:59 +0500

    Update index.js

--- index.js
+++ index.js
@@ -6,28 +6,35 @@ const cors=require('cors');
 const port = 3000;
 const user=require('./Api/Routes/user_api')
 const social_media=require('./Api/Routes/social_media')
+const userImage=require('./Api/Routes/index')
+const flickCode=require('./Api/Routes/flickCodes')
 const bodyparser=require('body-parser');
 app.use(bodyparser.urlencoded({extended:false}));
 app.use(bodyparser.json());
-app.use('/user', user);
+app.use('/user', user); 
 app.use('/socialmedia', social_media);
- // Handle 404 - Route Not Found
-app.use(cors());
- app.use((req, res, next) => {
+app.use('/userImage',userImage)
+app.use('/flickCode',flickCode);
+app.use(express.static('public'));
+ 
+
+app.use(cors);
+
+ app.use((req, res, next) => { 
   res.status(404).json({
     error: 'Bad Request'
   });
 });
-
+ 
 const uri = "mongodb+srv://mfahimch:78678PPIred@cluster0.eviduti.mongodb.net/?retryWrites=true&w=majority";
 
-
+ 
 async function run() {
   try {
     const connectionString = "mongodb://localhost:27017/test";
 
     await mongoose.connect(uri, {
-      useNewUrlParser: true,
+      useNewUrlParser: true, 
       useUnifiedTopology: true,
     }).then(data => {
       console.log('data');
