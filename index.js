830f4f58cf05664f738cbbd153dd7bb001edd634
MFahim786 <132273833+MFahim786@users.noreply.github.com>
2024-01-21 20:21:04 +0500

    Update index.js

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ app.use('/socialmedia', social_media);
 app.use('/UserImg', userImage);
 app.use('/flickCode',flickCode);
 app.use(express.static('public'));
-app.use(cors());
+app.use(cors()); 
 
  app.use((req, res, next) => { 
   res.status(404).json({
