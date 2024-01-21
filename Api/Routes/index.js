33ddf249a17ed23bd053c4562dd977a8f79b34e7
MFahim786 <132273833+MFahim786@users.noreply.github.com>
2024-01-21 20:11:49 +0500

    Update index.js

--- Api/Routes/index.js
+++ Api/Routes/index.js
@@ -1,21 +1,21 @@
 const express = require("express");
-const app = express();
+const router = express.Router();
 const bodyParser = require("body-parser");
 const multer = require("multer");
-const cors = require("cors");
-const fs = require("fs");
-const path = require("path");
+const { v2 } = require('cloudinary');
 const User = require("./Model/user_model");
-app.use(cors());
-const router = require("express").Router();
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
+const cors = require("cors");
+
+router.use(cors());
+router.use(bodyParser.urlencoded({ extended: false }));
+router.use(bodyParser.json());
+
 const storage = multer.diskStorage({
   destination: (req, file, cb) => {
     cb(null, "./public/temp");
   },
   filename: (req, file, cb) => {
-    cb(null, file.originalname);
+    cb(null, file.originalname); 
   },
 });
 
@@ -27,7 +27,7 @@ const fileFilter = (req, file, cb) => {
     cb(new Error("Only JPEG and PNG files are allowed"), false);
   }
 };
- 
+
 const upload = multer({
   storage: storage,
   limits: {
@@ -35,60 +35,51 @@ const upload = multer({
   },
   fileFilter: fileFilter,
 });
+
 router.put("/uploadUserImg/:id", upload.single("testImage"), async (req, res) => {
   try {
-    // Find the user by the provided id
     const userId = req.params.id;
-    console.log("req.params.id:", req.params.id);
-    const user = await User.findOne({ id: userId });
-
-    if (!user) {
-      return res.status(404).json({ error: "User not found" });
-    } 
-    console.log("req.file:", req.file);
-
-    // Save the image data associated with the user
-    const saveImage = {
-      name: req.body.name,
-      img: {
-        data: fs.readFileSync("./public/temp/" + req.file.filename),
-        contentType: "image/png",
-      },
-    };
-     
-    // Update the user's imageData field with the saved image data
-    user.imageData = saveImage;
-    
-    await user.save();
-    console.log("Image is saved"); 
-    res.send("Image is saved");
-  } catch (err) { 
-    console.error("Error saving image:", err);
-    res.status(500).send("Error saving image");
-  }
-});
-router.get("/fetchUserImg/:id", async (req, res) => {
-  try {
-    const userId = req.params.id;
- 
-    // Find the user by the provided id
     const user = await User.findOne({ id: userId });
 
     if (!user) {
       return res.status(404).json({ error: "User not found" });
     }
 
-    // Respond with user information including image data
+    const pathimg = req.file.path;
+
+    v2.config({
+      cloud_name: 'dxyhmgghf',
+      api_key: '315569673652811',
+      api_secret: '2vWMo0Od99h_maW8P-9DKjc_Neo'
+    });
+
+    const result = await v2.uploader.upload(pathimg, { resource_type: "auto" });
+    console.log(result.url);
+
+    user.userImage = result.url;
+    const usersave = await user.save();
+
+    console.log('usersave', usersave);
+    res.send("Image is saved");
+  } catch (err) {
+    console.error("Error saving image:", err);
+    res.status(500).send("Error saving image");
+  }
+});
+
+router.get("/fetchUserImg/:id", async (req, res) => {
+  try {
+    const userId = req.params.id;
+    const user = await User.findOne({ id: userId });
+
+    if (!user) {
+      return res.status(404).json({ error: "User not found" });
+    }
+
     res.json({
       id: user.id,
       name: user.name,
-      email: user.email,
-      // ... other user properties
-      imageData: {
-        name: user.imageData.name,
-        contentType: user.imageData.img.contentType,
-        data: user.imageData.img.data.toString("base64"), // Convert binary data to base64
-      },
+      UserImage: user.userImage,
     });
   } catch (err) {
     console.error("Error fetching user:", err);
