ef6c2ebfc98cfac2a2f8e38ec28209fd447d667d
MFahim786 <132273833+MFahim786@users.noreply.github.com>
2024-01-22 14:12:37 +0500

    Update user_api.js

--- Api/Routes/user_api.js
+++ Api/Routes/user_api.js
@@ -31,6 +31,7 @@ router.get('/:id', async (req, res, next) => {
         isActive: user.isActive,
         isLost:user.isLost,
         lostMassege:user.lostMassege,
+        userDirectMode:user.userDirectMode
         socialMedia: user.socialMedia || []
       }
     });
