b4b63a2f012420a1cbd258b7d719c0508799e49c
MFahim786 <132273833+MFahim786@users.noreply.github.com>
2024-01-27 17:04:25 +0500

    Update user_model.js

--- Api/Routes/Model/user_model.js
+++ Api/Routes/Model/user_model.js
@@ -21,7 +21,8 @@ const socialMediaSchema = new mongoose.Schema({
   },
   isActive: {
     type: Boolean,
-    required: false
+    required: false,
+   default: false 
   },
   socialMediaDirectMode: {
     type: Boolean,
