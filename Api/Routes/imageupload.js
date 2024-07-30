c7076164f76b18ffc53bb0df0936c67c01fce63c
flicktags <159721553+flicktags@users.noreply.github.com>
2024-07-31 01:18:30 +0500

    Update imageupload.js

--- Api/Routes/imageupload.js
+++ Api/Routes/imageupload.js
@@ -4,9 +4,9 @@ const cloudinary = require("cloudinary").v2;
 const streamifier = require('streamifier');
 const User = require("./Model/user_model");
 cloudinary.config({
-  cloud_name: 'diwspe6yi',
-  api_key: '457834299199625',
-  api_secret: 'cOt4I5PxEZ7fPWOI0vmzXYzLt6o',
+  cloud_name: 'dxyhmgghf',
+  api_key: '315569673652811',
+  api_secret: '2vWMo0Od99h_maW8P-9DKjc_Neo',
   secure: true,
 });
 
@@ -19,7 +19,7 @@ function runMiddleware(req, res, fn) {
         return reject(result);
       }
       return resolve(result);
-    });  
+    });
   });
 }
 
@@ -28,7 +28,7 @@ async function handleImageUpload(req, res) {
   try {
     await runMiddleware(req, res, upload);
 
-    // console.log(req.file.buffer);
+    console.log(req.file.buffer);
     const userId = req.params.id;
     const user = await User.findOne({ id: userId });
 
@@ -58,6 +58,40 @@ async function handleImageUpload(req, res) {
     res.status(500).json({ error: 'Internal Server Error' });
   }
 }
+async function userBannerImage(req, res) {
+  try {
+    await runMiddleware(req, res, upload);
 
-module.exports = { runMiddleware, handleImageUpload };
+    console.log(req.file.buffer);
+    const userId = req.params.id;
+    const user = await User.findOne({ id: userId });
+
+    if (!user) {
+      return res.status(404).json({ error: "User not found" });
+    }
+    const stream = cloudinary.uploader.upload_stream(
+      { folder: "flick-app-userimage" },
+     async (error, result) => {
+        if (error) {
+          console.error(error);
+          res.status(500).json({ error: 'Internal Server Error' });
+        } else {
+          console.log(result.url);
+
+           user.userBannerImage = result.url;
+           const usersave = await user.save();
+           res.status(200).json("User Banner Image saved");
+
+        }
+      }
+    );
+
+    streamifier.createReadStream(req.file.buffer).pipe(stream);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: 'Internal Server Error' });
+  }
+}
+
+module.exports = { runMiddleware, handleImageUpload,userBannerImage };
 
